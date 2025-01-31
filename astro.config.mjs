33c1491847d9eb8f671bf5f4d24a4ef487f9fafc
Manuel Retamozo <michael.retamozo@gmail.com>
2025-02-01 00:15:17 +0100

    guardado 01-02

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind({
+      config: "./tailwind.config.mjs",
+    }),
+  ],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+  },
+  output: "hybrid",
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	darkMode: 'class',
-	important: true,
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    backgroundColor: {
+      "main--gradient": "linear-gradient(90deg, #0D7490, #121212)",
+      "dark--card": "rgb(18, 18, 18)",
+      "essential-subdued": "#727272",
+      "essential-bright-accent": "#1ed760",
+      "essential-negative": "#e91429",
+      "essential-warning": "#ffa42b",
+      "essential-positive": "#1ed760",
+      "essential-announcement": "#0d72ea",
+    },
+    height: {
+		"page": "100dvh"
+	}
+  },
+  darkMode: "class",
+  plugins: [],
+};
